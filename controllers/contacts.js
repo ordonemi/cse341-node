20824d9013a6b45ebda7f027b52d61e1e015a523
Emilio Ordonez Guerrero <emilioordgue23@gmail.com>
2024-05-11 19:30:38 -0600

    personal assignment 3

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,25 +1,71 @@
+const { response } = require('express');
 const mongodb = require('../database/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
-    const result = await mongodb.getDb().db('sample_mflix').collection('contacts').find();
-    result.toArray().then((lists) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists);
-    });
-  };
-
-const getSingle = async (req, res, next) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb
-        .getDb()
-        .db('sample_mflix')
-        .collection('contacts')
-        .find({_id: userId});
-    result.toArray().then((lists) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists[0]);
-    });
+  const result = await mongodb.getDb().db('sample_mflix').collection('contacts').find();
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists);
+  });
 };
 
-module.exports = { getAll, getSingle };
+const getSingle = async (req, res, next) => {
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDb()
+    .db('sample_mflix')
+    .collection('contacts')
+    .find({ _id: userId });
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists[0]);
+  });
+};
+
+const addContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const result = await mongodb.getDb().db('sample_mflix').collection('contacts').insertOne(contact);
+  if (result.acknowledged){
+    res.status(201).json(result);
+  } else {
+    res.status(500).json(result.error || 'Some error occurred while creating the contact.');
+  }
+};
+
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const result = await mongodb.getDb().db('sample_mflix').collection('contacts').replaceOne({ _id: userId }, contact);
+  console.log(result);
+  if (result.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(result.error || 'Some error occurred while updating the contact.');
+  }
+};
+
+const deleteContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb.getDb().db('sample_mflix').collection('contacts').deleteOne({ _id: userId }, true);
+  console.log(result);
+  if (result.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(result.error || 'Some error occurred while deleting the contact.');
+  }
+};
+
+module.exports = { getAll, getSingle, addContact, updateContact, deleteContact  };
--- routes/contacts.js
+++ routes/contacts.js
@@ -3,8 +3,19 @@ const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
 
+// Get all contacts
 router.get('/', contactsController.getAll);
 
+// Get specific contact based on ID
 router.get('/:id', contactsController.getSingle);
+
+// Create new contact
+router.post('/',contactsController.addContact);
+
+// Update contact
+router.put('/:id', contactsController.updateContact);
+
+// Delete contact
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
