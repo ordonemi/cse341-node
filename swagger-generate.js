84677705ff39014e73e3255ac58fb342bfe572cb
Emilio Ordonez Guerrero <emilioordgue23@gmail.com>
2024-05-18 17:46:55 -0600

    asdf

--- swagger-generate.js
+++ swagger-generate.js
@@ -5,10 +5,10 @@ const doc = {
     title: 'Contacts API',
     description: 'Mongo DB collection named contacts that holds contact information.'
   },
-  host: ['localhost:8080', 'cse341-node-dj67.onrender.com']
+  host: ['cse341-node-dj67.onrender.com']
 };
 
-const outputFile = './swagger-output.json';
-const routes = ['./server.js'];
+const outputFile = './swagger.json';
+const routes = ['./routes/index.js'];
 
 swaggerAutogen(outputFile, routes, doc);
